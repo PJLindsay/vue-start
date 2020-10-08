25e81927c519bcfc977d60fd1985012fdee50283
PJ Lindsay <pjlindsay@gmail.com>
2020-10-08 14:05:07 -0600

    Setup basic app

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'jim',
+          name: 'Jim Studious',
+          phone: '555 444 3333',
+          email: 'jim@localhost.com'
+        },
+        {
+          id: 'mary',
+          name: 'Mary Moxie',
+          phone: '555 223 3344',
+          email: 'mary@localhost.com'
+        },
+        {
+          id: 'eduina',
+          name: 'Eduina Mitchell',
+          phone: '535 723 9999',
+          email: 'eduina@localhost.com'
+        }
+      ]
+    }
+  }
+}
+</script>
