f5ddb8742fb6737164c2066801807f58eb76b513
PJ Lindsay <pjlindsay@gmail.com>
2020-10-09 09:18:28 -0600

    Add Add Contact component

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App.vue'
 import FriendContact from './components/FriendContact.vue'
+import AddEditContact from './components/AddEditContact.vue'
 
 const app = createApp(App)
 
 app.component('friend-contact', FriendContact)
+app.component('addedit-contact', AddEditContact)
 
 app.mount('#app');
